a5e9b44774160bf87c5b2c240334f9157f168942
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-17 09:54:08 +0300

    Tüm entityler için seed datalar oluşturuldu

---
+++ Nlayer/Nlayer.Data/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nlayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Data.Seeds
+{
+    public class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "kirmizi",
+                Height = 200,
+                Weight = 100,
+                ProductId = 1,
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "mavi",
+                Height = 200,
+                Weight = 300,
+                ProductId = 2,
+            });
+        }
+    }
+}
---
+++ Nlayer/Nlayer.Data/Seeds/ProductSeed.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nlayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Data.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product()
+            {
+                Id = 1,
+                Name = "Kalem1",
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now,
+
+
+
+            }, new Product()
+            {
+                Id = 1,
+                Name = "Kalem2",
+                CategoryId = 1,
+                Price = 200,
+                Stock = 20,
+                CreatedDate = DateTime.Now,
+            },
+            new Product()
+            {
+                Id = 1,
+                Name = "Kalem3",
+                CategoryId = 1,
+                Price = 5000,
+                Stock = 20,
+                CreatedDate = DateTime.Now,
+            },
+            new Product()
+        {
+                      Id = 4,
+                 Name = "kitap1",
+                 CategoryId = 2,
+                 Price = 5000,
+                 Stock = 20,
+                 CreatedDate = DateTime.Now,
+         },
+               new Product()
+               {
+                   Id = 4,
+                   Name = "kitap2",
+                   CategoryId = 2,
+                   Price = 5000,
+                   Stock = 20,
+                   CreatedDate = DateTime.Now,
+               }
+
+            );
+        }
+    }
+}
