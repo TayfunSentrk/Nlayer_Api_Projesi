17f845ddce24ed622f9419d88cb0b90031bba369
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-17 16:40:51 +0300

    ProductUpdateDto oluşturuldu

---
+++ Nlayer/Nlayer.Core/Dtos/ProductUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Dtos
+{
+    public class ProductUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // stock property oluşturuldu
+        public int Stock { get; set; }
+        // Price  property oluşturuldu parasal birim olduğu için decimak oluşturuldu
+        public decimal Price { get; set; }
+        // Category ile product arasında bire çok ilişki olduğu için CategoryId property eklendi
+        public int CategoryId { get; set; }
+        // Category ile product arasında bire çok ilişki olduğu için Category Navigation Property oluşturuldu
+    }
+}
