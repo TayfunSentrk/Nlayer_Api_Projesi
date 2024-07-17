e4a3c44309d4162cbae1b5f59774bf6501e7f542
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-17 14:01:16 +0300

    Appsetting json'da yer alan  sql serverin program cs'te yolu verildi.Dependency enjection unitofwork ve generic repository için uygulandı

--- Nlayer/Nlayer.API/Program.cs
+++ Nlayer/Nlayer.API/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Nlayer.Core.Repositories;
+using Nlayer.Core.UnitOfWorks;
+using Nlayer.Data;
+using Nlayer.Data.Repositories;
+using Nlayer.Data.UnitOfWorks;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +15,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>(); //IUnitwýrk gördüðü yerde unitof work kullanýcak
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>)); //generic olduðu için type of þekinde ekledim
+
+builder.Services.AddDbContext<AppDbContext>(options => //sql yolu appsetting json'dan verildi
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext))!.GetName().Name); //burda migration oluþcaðý yer yani appdbcontext assembly ! bunu null olmacaðýný belirrttim
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
