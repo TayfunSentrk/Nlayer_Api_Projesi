ba01f0385db729e46d121e040ecfc272b1efcca2
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-17 13:42:38 +0300

    UnitWork implemente edildi.

---
+++ Nlayer/Nlayer.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Nlayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        /// <summary>
+        /// Veritabanına kaydetmek için merkezi bir yerden asekron olmayan savechanges methodunu uygular
+        /// </summary>
+        
+        /// <returns>Herhangi bir şeye geri dönmeyen method </returns>
+        public void Commit()
+        {
+           _appDbContext.SaveChanges();
+        }
+        /// <summary>
+        /// Veritabanına kaydetmek için merkezi bir yerden asekron olan savechangesAsync methodunu uygular
+        /// </summary>
+
+        /// <returns> Asekron bir işlem sonucunda Herhangi bir şeye geri dönmeyen method </returns>
+        public async Task CommitAsync()
+        {
+           await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
