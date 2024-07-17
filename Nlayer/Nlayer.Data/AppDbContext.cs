4817b103a01f679ee41b86defaeae459ae4b98d3
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-17 09:13:37 +0300

    Dbset yapıları oluşturuldu.Veritabanında oluşucak tabloların isimleri verildi

--- Nlayer/Nlayer.Data/AppDbContext.cs
+++ Nlayer/Nlayer.Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Nlayer.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace Nlayer.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+
+        // Veritabanından oluşucak tabloların isimleri verildi
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+
+        // veritabanı yolunu program cs'te vermek için bunu yaptım
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        
     }
 }
