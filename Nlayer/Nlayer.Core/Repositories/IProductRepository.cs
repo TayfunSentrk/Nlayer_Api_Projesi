72c6de25ffa4ac5aad4eee1e21a61df59e90e9bb
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 09:38:58 +0300

    IProductRepository oluşturdu.IGenericRepository'de ortak olanlar dahil edildi.Bunun yanısıra product'a ait method yazıldı

---
+++ Nlayer/Nlayer.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Nlayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Repositories
+{
+    /// <summary>
+    /// Ürünlerle ilgili özel repository arayüzü
+    /// </summary>
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        /// <summary>
+        /// Kategorileriyle birlikte ürünleri getiren metot
+        /// </summary>
+        /// <returns>Kategorileriyle birlikte ürünlerin listesini döner</returns>
+        Task<List<Product>> GetProductWithCategory();
+    }
+}
