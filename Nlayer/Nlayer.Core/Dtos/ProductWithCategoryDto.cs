9ee389a3d57f4f05ee8be4b6f86ea20d7b2ff262
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 10:08:11 +0300

    ProductWithCategoryDto oluşturuldu.ProductDto'dan gelen özellikler miras alındı.Bunun yanısıra Category getirmek için CategoryDto eklendi

---
+++ Nlayer/Nlayer.Core/Dtos/ProductWithCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using Nlayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Dtos
+{
+    public class ProductWithCategoryDto:ProductDto
+    {
+        //ProductDto 'dan miras alındı .Categori'yi çekmek için CategoryDto'dakiler class olarak oluşturuldu
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
