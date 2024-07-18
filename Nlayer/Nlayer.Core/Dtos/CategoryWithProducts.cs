5f60818577a9c0814b6df312657a7ace3cf610dd
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 14:26:58 +0300

    ICategoryService oluştruldu.IService'ten ortak methodlar miras alındı.Category'e ait yeni bir method yazıldı

---
+++ Nlayer/Nlayer.Core/Dtos/CategoryWithProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Dtos
+{
+    public class CategoryWithProducts:CategoryDto
+    {
+      
+        public List<ProductDto> ProductDtos { get; set; }
+    }
+}
---
+++ Nlayer/Nlayer.Core/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Nlayer.Core.Dtos;
+using Nlayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Services
+{
+    public interface ICategoryService:IService<Category>
+    {
+
+        public Task<CustomResponseDto<CategoryWithProducts>> GetSingleCategoryByIdWithProductAsync(int categoryId);
+    }
+}
