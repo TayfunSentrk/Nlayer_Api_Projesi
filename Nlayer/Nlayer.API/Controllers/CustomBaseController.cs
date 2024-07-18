2e4ce7d95d51ac5aea4a4eaa5b4dd4b6bef7e754
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 09:23:01 +0300

    Product Controller oluşturuldu.Crud işlemleri için ilgili methodlar yazıldı

---
+++ Nlayer/Nlayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.Dtos;
+
+namespace Nlayer.API.Controllers
+{
+
+    /// <summary>
+    /// Özel taban kontrolcü sınıfı
+    /// </summary>
+    public class CustomBaseController : ControllerBase
+    {
+        /// <summary>
+        /// Bir end point olmadığını göstermek için NonAction olarak işaretlenmiş metot
+        /// </summary>
+        /// <typeparam name="T">Dönüş tipi</typeparam>
+        /// <param name="response">CustomResponseDto türünde yanıt</param>
+        /// <returns>IActionResult türünde yanıt</returns>
+        [NonAction] // bir end point olmadığını göstermek için
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            // Eğer yanıtın durum kodu 204 ise, içeriği boş bir yanıt döner
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null) { StatusCode = response.StatusCode };
+            }
+
+            // Yanıtı ve durum kodunu döner
+            return new ObjectResult(response) { StatusCode = response.StatusCode };
+        }
+    }
+}
---
+++ Nlayer/Nlayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.Dtos;
+using Nlayer.Core.Models;
+using Nlayer.Core.Services;
+
+namespace Nlayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    /// <summary>
+    /// Ürünlerle ilgili işlemleri gerçekleştiren kontrolcü sınıfı
+    /// </summary>
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper mapper;
+        private readonly IService<Product> service;
+
+        /// <summary>
+        /// IMapper ve IService<Product> bağımlılıklarını enjekte eden kurucu metot
+        /// </summary>
+        /// <param name="mapper">IMapper nesnesi</param>
+        /// <param name="service">IService<Product> nesnesi</param>
+        public ProductsController(IMapper mapper, IService<Product> service)
+        {
+            this.mapper = mapper;
+            this.service = service;
+        }
+
+        /// <summary>
+        /// Tüm ürünleri getiren metot
+        /// </summary>
+        /// <returns>Ürünlerin listesini döner</returns>
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            // Tüm ürünleri al
+            var products = await service.GetAll();
+            // Ürünleri DTO'ya dönüştür
+            var productsDto = mapper.Map<List<ProductDto>>(products.ToList());
+
+            // Başarılı sonuç döner
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDto));
+        }
+
+        /// <summary>
+        /// Belirli bir ID'ye sahip ürünü getiren metot
+        /// </summary>
+        /// <param name="id">Ürünün ID'si</param>
+        /// <returns>Ürün bilgilerini döner</returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            // ID'ye göre ürünü al
+            var product = await service.GetByIdAsync(id);
+            // Ürünü DTO'ya dönüştür
+            var productDto = mapper.Map<ProductDto>(product);
+
+            // Başarılı sonuç döner
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
+        }
+
+        /// <summary>
+        /// Yeni bir ürün kaydeden metot
+        /// </summary>
+        /// <param name="product">Kaydedilecek ürün DTO'su</param>
+        /// <returns>Kaydedilen ürün bilgilerini döner</returns>
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto product)
+        {
+            // Yeni ürünü ekle
+            var createdProduct = await service.AddAsync(mapper.Map<Product>(product));
+            // Eklenen ürünü DTO'ya dönüştür
+            var productDto = mapper.Map<ProductDto>(createdProduct);
+
+            // Başarılı sonuç döner
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productDto));
+        }
+
+        /// <summary>
+        /// Mevcut bir ürünü güncelleyen metot
+        /// </summary>
+        /// <param name="product">Güncellenecek ürün DTO'su</param>
+        /// <returns>Başarılı sonuç döner (içerik yok)</returns>
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto product)
+        {
+            // Ürünü güncelle
+            await service.UpdateAsync(mapper.Map<Product>(product));
+
+            // Başarılı sonuç döner (içerik yok)
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        /// <summary>
+        /// Belirli bir ID'ye sahip ürünü silen metot
+        /// </summary>
+        /// <param name="id">Silinecek ürünün ID'si</param>
+        /// <returns>Başarılı sonuç döner (içerik yok)</returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            // ID'ye göre ürünü al
+            var product = await service.GetByIdAsync(id);
+            // Ürünü sil
+            await service.RemoveAsync(product);
+
+            // Başarılı sonuç döner (içerik yok)
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+    }
+
+}
