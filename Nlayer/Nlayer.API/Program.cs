10a56729dbf958e90a103117ae64599e9e849621
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 15:46:51 +0300

    program cs' tarafında ApiBehaviorOptions ile api'nin default gelen modelinvalid response durumu pasif hale getirildi.Böylece CustomReponse aktif hale gelicek

--- Nlayer/Nlayer.API/Program.cs
+++ Nlayer/Nlayer.API/Program.cs
@@ -1,4 +1,5 @@
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Nlayer.API.Filters;
 using Nlayer.Core.Repositories;
@@ -22,6 +23,11 @@ builder.Services.AddControllers(options=> { options.Filters.Add(new ValidateFilt
     opt.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>();
 });//burda productdtovalidotor assembly aldýk
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;// framework belirlemiþ olduðu response baskýladýk
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
