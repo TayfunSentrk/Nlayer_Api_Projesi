f92630dea068b6f965c2f77bb67b78a58e66f840
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 14:11:25 +0300

    ICategoryRepository oluşuturuldu.IGenericRepository ortak methodları miras aldı.Custom Method tanımlandı category için.

---
+++ Nlayer/Nlayer.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Nlayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Repositories
+{
+    /// <summary>
+    /// ICategoryRepository arayüzü, kategori verilerini yönetmek için gerekli metodları tanımlar.
+    /// GetSingleCategoryByIdWithProduct metodu, belirli bir kategori ve bu kategoriye ait ürünleri getirir.
+    /// </summary>
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        /// <summary>
+        /// GetSingleCategoryByIdWithProduct metodu, belirli bir kategori ve bu kategoriye ait ürünleri getirir.
+        /// </summary>
+        /// <param name="categoryId">Getirilecek kategorinin ID'si.</param>
+        /// <returns>
+        /// Belirtilen ID'ye sahip kategori ve bu kategoriye ait ürünlerin bulunduğu bir liste döner.
+        /// Kategori, ilişkili ürünleri içeren tek bir Category nesnesi olarak döner.
+        /// Örneğin:
+        /// - Category
+        ///     - ProductA
+        ///     - ProductB
+        /// </returns>
+        public Task<List<Category>> GetSingleCategoryByIdWithProduct(int categoryId);
+    }
+
+}
