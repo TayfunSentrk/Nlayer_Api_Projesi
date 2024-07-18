304e8b0b86b1afb04cdbef48796903ad7c380f0d
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 14:41:53 +0300

    Category Controller oluşturuldu.Category ait product'ların getirilmesi için ilgili end point yazıldı

--- Nlayer/Nlayer.API/Controllers/CategoriesController.cs
+++ Nlayer/Nlayer.API/Controllers/CategoriesController.cs
@@ -1,11 +1,50 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.Services;
 
 namespace Nlayer.API.Controllers
 {
+
+    /// <summary>
+    /// CategoriesController sınıfı, kategori verilerini yönetmek için gerekli API endpoint'lerini tanımlar.
+    /// CustomBaseController sınıfından türetilmiştir.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : CustomBaseController
     {
+        private readonly ICategoryService _categoryService;
+
+        /// <summary>
+        /// CategoriesController yapıcı metodu, kategori servisini alır.
+        /// </summary>
+        /// <param name="categoryService">Kategori servisi</param>
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        /// <summary>
+        /// Belirli bir kategori ve bu kategoriye ait ürünleri getirir.
+        /// </summary>
+        /// <param name="id">Getirilecek kategorinin ID'si</param>
+        /// <returns>
+        /// Belirtilen ID'ye sahip kategori ve bu kategoriye ait ürünlerin bulunduğu CategoryWithProducts DTO'sunu içeren bir IActionResult döner.
+        /// Örneğin:
+        /// - CategoryWithProducts
+        ///     - CategoryId
+        ///     - CategoryName
+        ///     - Products
+        ///         - ProductA
+        ///         - ProductB
+        /// </returns>
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> GetCategoryByIdWithProducts(int id)
+        {
+            // Kategori ID'sine sahip kategoriyi ve bu kategoriye ait ürünleri getirir,
+            // daha sonra sonucu CustomResponseDto formatında döner.
+            var result = await _categoryService.GetSingleCategoryByIdWithProductAsync(id);
+            return CreateActionResult(result);
+        }
     }
 }
