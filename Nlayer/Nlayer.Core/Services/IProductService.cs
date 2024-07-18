7838626c024469d87279e8c5027e9c17a95f5db1
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-18 10:04:23 +0300

    IProductService oluşturuldu.Ortak Methodlar IServisten alındı

---
+++ Nlayer/Nlayer.Core/Services/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Nlayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Services
+{
+    /// <summary>
+    /// Ürünlerle ilgili servis arayüzü
+    /// </summary>
+    public interface IProductService : IService<Product>
+    {
+        /// <summary>
+        /// Kategorileriyle birlikte ürünleri asenkron olarak getiren metot
+        /// </summary>
+        /// <returns>Kategorileriyle birlikte ürünlerin listesini döner</returns>
+        Task<List<Product>> GetProductsWithCategoryAsync();
+    }
+}
