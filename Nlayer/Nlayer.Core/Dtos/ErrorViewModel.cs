36b006ba49f64b6d5ec80f78f090087fd0df078e
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-19 16:37:11 +0300

    Mvc Katmanı için ErrorViewModel oluşturuldu

---
+++ Nlayer/Nlayer.Core/Dtos/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Core.Dtos
+{
+    public class ErrorViewModel
+    {
+        //mvc katmannı error viewmodel oluşturuldu
+        public List<string> Errors {  get; set; }   
+    }
+}
