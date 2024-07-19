57abc83fc63f10907761e3df1e75359ef1fc4304
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-19 12:30:11 +0300

    Api'de yapılan program cs' için gerekli işlemler mvc katmanında memorycache,automapper,appdbcontext,autofac enjection yapıldı

--- Nlayer/Nlayer.Web/Program.cs
+++ Nlayer/Nlayer.Web/Program.cs
@@ -1,7 +1,34 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Nlayer.Web.Modules;
+using Microsoft.EntityFrameworkCore;
+using Nlayer.Data;
+using System.Reflection;
+using Nlayer.Service.Mapping;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMemoryCache();//cache yapýsýný dahil etmek için ekledim
+ 
+builder.Services.AddAutoMapper(typeof(MapProfile));//mapprofile'nin içinde bulunduðu assembly 
+
+builder.Services.AddDbContext<AppDbContext>(options => //sql yolu appsetting json'dan verildi
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext))!.GetName().Name); //burda migration oluþcaðý yer yani appdbcontext assembly ! bunu null olmacaðýný belirrttim
+    });
+});
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()); //kütüphanden gelen servis eklendi
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => //containerbuilder autofacten geliyor burda autofac'ta yaptýðýmýz uygulamalarý buraya dahil ettik
+{
+    containerBuilder.RegisterModule(new RepoServiceModule());
+});
+
 
 var app = builder.Build();
 
