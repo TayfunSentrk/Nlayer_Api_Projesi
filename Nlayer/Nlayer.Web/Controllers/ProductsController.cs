d4949a99198289f5fde24fbf6461b635e0559a18
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-19 12:01:56 +0300

    Mvc Katmanında ProductsController oluşturuldu

---
+++ Nlayer/Nlayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nlayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
