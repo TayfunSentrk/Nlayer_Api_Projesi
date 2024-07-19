7efbffe7fb7ed6e0a4f3b95225fd3212b0c11333
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-19 14:57:29 +0300

    Ürün ekleme için end point yazıldı.Select option tagında categorilerin gelmesi için category servisi çağırıldı

--- Nlayer/Nlayer.Web/Controllers/ProductsController.cs
+++ Nlayer/Nlayer.Web/Controllers/ProductsController.cs
@@ -1,32 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nlayer.Core.Dtos;
+using Nlayer.Core.Models;
 using Nlayer.Core.Services;
 
 namespace Nlayer.Web.Controllers
-{
-    /// <summary>
-    /// Ürünlerle ilgili işlemleri yöneten kontrolcü.
-    /// </summary>
+{/// <summary>
+ /// Ürün kaydetme işlemlerini yöneten kontrolcü.
+ /// </summary>
     public class ProductsController : Controller
     {
         private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+        private readonly IMapper mapper;
 
         /// <summary>
         /// <see cref="ProductsController"/> sınıfının yeni bir örneğini başlatır.
         /// </summary>
         /// <param name="productService">Ürün servis örneği.</param>
-        public ProductsController(IProductService productService)
+        /// <param name="categoryService">Kategori servis örneği.</param>
+        /// <param name="mapper">Nesne dönüştürücü örneği.</param>
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
         {
             this.productService = productService;
+            this.categoryService = categoryService;
+            this.mapper = mapper;
         }
 
         /// <summary>
-        /// Ürünlerin ve kategorilerinin listesini görüntüler.
+        /// Ürün ekleme sayfasını görüntüler.
         /// </summary>
-        /// <returns>Ürünlerin ve kategorilerinin listesi ile görünümü döner.</returns>
-        public async Task<IActionResult> Index()
+        /// <returns>Ürün ekleme sayfasını döner.</returns>
+        [HttpGet]
+        public async Task<IActionResult> Save()
         {
-            var response = await productService.GetProductsWithCategoryAsync();
-            return View(response.Data);
+            var categories = mapper.Map<List<CategoryDto>>(await categoryService.GetAll());
+            ViewBag.categories = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+
+        /// <summary>
+        /// Yeni bir ürünü kaydeder.
+        /// </summary>
+        /// <param name="productDto">Kaydedilecek ürün bilgileri.</param>
+        /// <returns>İşlem başarılıysa ürün listesini döner, değilse ürün ekleme sayfasını döner.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await productService.AddAsync(mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = mapper.Map<List<CategoryDto>>(await categoryService.GetAll());
+            ViewBag.categories = new SelectList(categories, "Id", "Name");
+
+            return View();
         }
     }
 
