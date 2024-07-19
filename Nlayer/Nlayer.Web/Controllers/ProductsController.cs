120f873699c358a41da381ea4d95ab12133b8f34
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-19 12:21:09 +0300

    Product Controllerda her productların listelenmesi için method yazıldı.View Index Sayfasında gerekli model alınarak bir tablo yapısı oluşturuldu

--- Nlayer/Nlayer.Web/Controllers/ProductsController.cs
+++ Nlayer/Nlayer.Web/Controllers/ProductsController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.Services;
 
 namespace Nlayer.Web.Controllers
 {
+    /// <summary>
+    /// Ürünlerle ilgili işlemleri yöneten kontrolcü.
+    /// </summary>
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService productService;
+
+        /// <summary>
+        /// <see cref="ProductsController"/> sınıfının yeni bir örneğini başlatır.
+        /// </summary>
+        /// <param name="productService">Ürün servis örneği.</param>
+        public ProductsController(IProductService productService)
         {
-            return View();
+            this.productService = productService;
+        }
+
+        /// <summary>
+        /// Ürünlerin ve kategorilerinin listesini görüntüler.
+        /// </summary>
+        /// <returns>Ürünlerin ve kategorilerinin listesi ile görünümü döner.</returns>
+        public async Task<IActionResult> Index()
+        {
+            var response = await productService.GetProductsWithCategoryAsync();
+            return View(response.Data);
         }
     }
+
 }
