fe5e11963a7751ea92c44b64b11aac0392c44db9
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-20 16:04:38 +0300

    CategoryWithDto Controller için Category listesi getirme ve Categori ekleme end pointleri yazıldı

---
+++ Nlayer/Nlayer.API/Controllers/CategoryWithDtoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.Dtos;
+using Nlayer.Core.Models;
+using Nlayer.Core.Services;
+
+namespace Nlayer.API.Controllers
+{
+
+    public class CategoryWithDtoController : CustomBaseController
+    {
+        private readonly IServiceWithDto<Category, CategoryDto> serviceWithDto;
+
+        public CategoryWithDtoController(IServiceWithDto<Category, CategoryDto> serviceWithDto)
+        {
+            this.serviceWithDto = serviceWithDto;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategory()
+        {
+            return CreateActionResult(await serviceWithDto.GetAll());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Save(CategoryDto categoryDto)
+        {
+            return CreateActionResult(await serviceWithDto.AddAsync(categoryDto));
+        }
+    }
+}
